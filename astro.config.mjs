473904a56b68151c3b6d400c7fad8203e21507c7
Ferreya Ricardo <ferreyra.gluti@gmail.com>
2024-02-04 13:21:11 -0300

    fix style/perfomance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://glutix.porfolio.github.io",
   base: "/porfolio",
+  integrations: [robotsTxt()],
 });
